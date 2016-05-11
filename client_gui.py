0d9450341108dd01fb592f363edda48fbd6aeebe
priyavishnu <mailpriyavishnu@gmail.com>
2016-05-11 12:37:43 -0700

    Integrated Arthi code, initial timer code of Priyanka's and have added few error handling in client

--- client_gui.py
+++ client_gui.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 """
 The client gui application
 """
@@ -8,6 +10,8 @@ import wx
 
 import socket
 import sys
+
+import time
 
 """
 The main panel that gives the login/signup information
@@ -69,38 +73,44 @@ class MainPanel(wx.Frame):
 	
 	print 'Server Format: ', serverFormat
 
-	sock.recv(1024)
-        sock.send(serverFormat)
-
-        
-	errCode = sock.recv(1024)
-	print "Error code : ", errCode
-	#errCode = "USER_NAME_ERROR"
-	if(errCode == "USER_NAME_ERROR" or errCode == "PASSWORD_ERROR" or errCode == "USER_NAME_TAKEN" or errCode == "ALREADY_LOGGED_IN"):
-		if(errCode == "USER_NAME_ERROR" or errCode == "PASSWORD_ERROR"):
-			errorMsg = wx.StaticText(self.panel, label="Wrong User name or password", 
-				pos = (150, 350))
-			errorMsg.SetForegroundColour((255,0,0))
-
-		elif(errCode == "USER_NAME_TAKEN"):
-			errorMsg = wx.StaticText(self.panel, label="User name taken", 
-				pos = (150, 350))
-			errorMsg.SetForegroundColour((255,0,0))
-
-		elif (errCode == "ALREADY_LOGGED_IN"):
-			errorMsg = wx.StaticText(self.panel, label="User already logged in", 
-				pos = (150, 350))
-			errorMsg.SetForegroundColour((255,0,0))
-
+	if(self.tc1.GetValue() == "" or self.tc2.GetValue() == ""):
+		errorMsg = wx.StaticText(self.panel, label="User name or password cannot be empty", 
+			pos = (150, 350))
+		errorMsg.SetForegroundColour((255,0,0))	
 		self.loginbut.Bind(wx.EVT_BUTTON, self.OnLogin)
 		self.signupbut.Bind(wx.EVT_BUTTON, self.OnSignup)
-	
+
 	else:
-		print "Success going to second panel"
-		app2 = SecondPanel(None, title = 'Mind Sync')
-		self.Hide()
-		app2.Show()
-        	self.Close(True) 
+	    	sock.recv(1024)
+            	sock.send(serverFormat)
+	    	errCode = sock.recv(1024)
+	    	print "Error code : ", errCode
+	    	#errCode = "USER_NAME_ERROR"
+	    	if(errCode == "USER_NAME_ERROR" or errCode == "PASSWORD_ERROR" or errCode == "USER_NAME_TAKEN" or errCode == "ALREADY_LOGGED_IN"):
+	        	if(errCode == "USER_NAME_ERROR" or errCode == "PASSWORD_ERROR"):
+				errorMsg = wx.StaticText(self.panel, label="Wrong User name or password", 
+					pos = (150, 350))
+				errorMsg.SetForegroundColour((255,0,0))
+
+			elif(errCode == "USER_NAME_TAKEN"):
+				errorMsg = wx.StaticText(self.panel, label="User name taken", 
+					pos = (150, 350))
+				errorMsg.SetForegroundColour((255,0,0))
+
+			elif (errCode == "ALREADY_LOGGED_IN"):
+				errorMsg = wx.StaticText(self.panel, label="User already logged in", 
+					pos = (150, 350))
+				errorMsg.SetForegroundColour((255,0,0))
+
+			self.loginbut.Bind(wx.EVT_BUTTON, self.OnLogin)
+			self.signupbut.Bind(wx.EVT_BUTTON, self.OnSignup)
+	
+		else:
+			print "Success going to second panel"
+			app2 = SecondPanel(None, title = 'Mind Sync')
+			self.Hide()
+			app2.Show()
+        		self.Close(True) 
 
 """
 The second panel that shows the word and the other player's response and takes in the input 
@@ -112,6 +122,7 @@ class SecondPanel(wx.Frame):
             size=(550, 700))
         self.pnl = wx.Panel(self)
 
+	#self.pnl.SetBackgroundColour((237,125,49))
         hbox = wx.BoxSizer(wx.HORIZONTAL)
 
 	print "Second panel"
@@ -145,27 +156,91 @@ class SecondPanel(wx.Frame):
 
 	self.word = wx.TextCtrl(self.pnl, style = wx.TE_PROCESS_ENTER, pos = (175,400), 
 	size = (200,50))
-
-
-	enterbut.Bind(wx.EVT_BUTTON, self.OnButtonPress)
 	
-
-	timerSizer = wx.StaticText(self.pnl, -1,label = "Timer : ", pos = (5,600))
-	timerfont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
-	timerSizer.SetFont(timerfont)
-
 	hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
         self.pnl.SetSizer(hbox)
         self.Centre()
         self.Show()
 
+	enterbut.Bind(wx.EVT_BUTTON, self.OnButtonPress)
+	
+	scoreString = "Score: "+words[1]
+        self.scoreSizer = wx.StaticText(self.pnl, -1,
+        label = scoreString , pos = (300, 630))
+        scorefont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        self.scoreSizer.SetFont(scorefont)
+        self.scoreSizer.SetForegroundColour((0,0,0))
+
+        responseString = words[2]+"'s response: "+words[3]
+        self.responseSizer = wx.StaticText(self.pnl, -1,
+        label = responseString , pos = (300, 650))
+        responsefont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        self.responseSizer.SetFont(responsefont)
+        self.responseSizer.SetForegroundColour((0,0,0))
+
+		
+	self.connectionErrorMsg = wx.StaticText(self.pnl,-1, label="Connected!!", pos = (300, 550))		
+	connectionErrorFont = wx.Font(15,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
+	self.connectionErrorMsg.SetFont(connectionErrorFont)
+	self.connectionErrorMsg.SetForegroundColour((255,0,0))
+	
+	self.gameEndErrorMsg = wx.StaticText(self.pnl, -1, label="Keep Playing!!!", pos = (15, 550))
+	gameEndFont = wx.Font(15,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
+	self.gameEndErrorMsg.SetFont(gameEndFont)
+	self.gameEndErrorMsg.SetForegroundColour((255,0,0))
+
+	self.timerSizer = wx.StaticText(self.pnl, -1,label = "Timer : ", pos = (5,600))
+	timerfont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+	self.timerSizer.SetFont(timerfont)
+	
+        self.timersz = wx.StaticText(self.pnl, -1, label="60",pos = (80,600))
+        self.timersz.SetFont(timerfont)
+
+        self.counter = 60
+
+        self.timer = wx.Timer(self)
+        self.Bind(wx.EVT_TIMER, self.onTimer, self.timer)
+        self.timer.Start(1000)
+
+    def onTimer(self, e):
+
+        self.counter -= 1
+        msg = "%s " % self.counter
+        self.timersz.SetLabel(msg)
+        if self.counter <= 0:
+            self.timer.Stop()   # stop the timer
+
+	
+
+
     def OnButtonPress(self, e):
 	print "Entered word: ", self.word.GetValue()
 	sock.send(self.word.GetValue())
-	response = sock.recv(1024)
+	print "receiving... "
+	try:
+		response = sock.recv(1024)
+	except:
+       		print "receive error or timedout"	
+		self.connectionErrorMsg.SetLabel("Connection error")
+		self.connectErrorMsg.SetForegroundColour((255,0,0))
+		time.sleep(5)
+		sys.exit(0)
 	words = response.split("_")
 	wrd = words[0]
-
+	print "receive end" + wrd
+	
+        #wrd = "GAMEEND"	
+	if (wrd == "GAMEEND"):
+       		print "received GAMEEND"	
+		gameErrormsg = "Your partner has closed the game, the game will exit now!!!"
+		self.gameEndErrorMsg.SetLabel(gameErrormsg)
+		gameEndFont = wx.Font(15,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
+		self.gameEndErrorMsg.SetFont(gameEndFont)
+		self.gameEndErrorMsg.SetForegroundColour((255,0,0))
+		#	time.sleep(5)
+		self.pnl.Destroy()
+	        sys.exit(0)
+	
 	self.wordSizer.SetLabel(wrd)
 	font = wx.Font(25,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
 	self.wordSizer.SetFont(font)
@@ -173,10 +248,21 @@ class SecondPanel(wx.Frame):
 	print "Word is" + wrd
 	#self.Close(True)
 
+	self.scoreSizer.SetLabel("Score: "+words[1])
+        scorefont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        self.scoreSizer.SetFont(scorefont)
+        self.scoreSizer.SetForegroundColour((0,0,0))
+
+        self.responseSizer.SetLabel(words[2]+"'s response: "+words[3])
+        responsefont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
+        self.responseSizer.SetFont(responsefont)
+        self.responseSizer.SetForegroundColour((0,0,0))
+
 
 if __name__ == '__main__':
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(30.0)
     host = socket.gethostbyname(sys.argv[1])
     mPort = 8777
     sPort = 8778
