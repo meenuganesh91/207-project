d804d856e86c773927452e5f4a2039bbb60a355c
meenuganesh91 <MeenuVimal@Meenus-MacBook-Pro.local>
2016-05-22 15:12:22 -0700

    Adding necessary comments

--- client_gui.py
+++ client_gui.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-"""
-The client gui application
-"""
+'''
+    The client gui application
+'''
 
 import wx
 import time
@@ -12,13 +12,13 @@ import sys
 USER_STATS = ""
 sock = 0
 
-"""
-The main panel that gives the login/signup information
-"""
+'''
+    The main panel that gives the login/signup information
+'''
 class MainPanel(wx.Frame):
-    """
-    The constructor for the main panel that displays the necessary widgets
-    """
+    '''
+        The constructor for the main panel that displays the necessary widgets
+    '''
     def __init__(self, parent, title):
 
         super(MainPanel, self).__init__(parent, title=title, 
@@ -32,25 +32,25 @@ class MainPanel(wx.Frame):
         userSizer = wx.StaticText(self.panel, label="User name", pos = (140, 250))
         passSizer = wx.StaticText(self.panel, label="Password", pos = (140, 300))
 
-	png = wx.Image("./index.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-	wx.StaticBitmap(self.panel, 1, png, (125, 5), (png.GetWidth(), png.GetHeight()))	
+        png = wx.Image("./index.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        wx.StaticBitmap(self.panel, 1, png, (125, 5), (png.GetWidth(), png.GetHeight()))
 
-	self.loginbut = wx.Button(self.panel, label='Login', pos=(50, 400))
+        self.loginbut = wx.Button(self.panel, label='Login', pos=(50, 400))
 
-	self.signupbut = wx.Button(self.panel, label='Sign up', pos=(350, 400))
+        self.signupbut = wx.Button(self.panel, label='Sign up', pos=(350, 400))
 
-	self.tc1 = wx.TextCtrl(self.panel, style = wx.TE_PROCESS_ENTER, pos = (250, 250))
-	bsizer = wx.BoxSizer()
-	bsizer.Add(self.tc1, 1, wx.EXPAND)
+        self.tc1 = wx.TextCtrl(self.panel, style = wx.TE_PROCESS_ENTER, pos = (250, 250))
+        bsizer = wx.BoxSizer()
+        bsizer.Add(self.tc1, 1, wx.EXPAND)
 
         self.tc2 = wx.TextCtrl(self.panel, style=wx.TE_PASSWORD|wx.TE_PROCESS_ENTER, pos = (250,300))
 
-	self.errorMsg = wx.StaticText(self.panel, label="Welcome to Mind Sync", 
+        self.errorMsg = wx.StaticText(self.panel, label="Welcome to Mind Sync",
 				pos = (150, 350))
-	self.errorMsg.SetForegroundColour((255,0,0))
+        self.errorMsg.SetForegroundColour((255,0,0))
 
-	self.loginbut.Bind(wx.EVT_BUTTON, self.OnLogin)
-	self.signupbut.Bind(wx.EVT_BUTTON, self.OnSignup)
+        self.loginbut.Bind(wx.EVT_BUTTON, self.OnLogin)
+        self.signupbut.Bind(wx.EVT_BUTTON, self.OnSignup)
 
         fgs.AddGrowableRow(2, 1)
         fgs.AddGrowableCol(1, 1)
@@ -58,46 +58,59 @@ class MainPanel(wx.Frame):
         hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
         self.panel.SetSizer(hbox)
         self.Centre()
-        self.Show()  
-
+        self.Show()
+    
+    '''
+        Function to establish connection with server on pressing login button
+    '''
     def OnLogin(self, e):
-	code = '2'
-	blank = ""
-	print "Trying to login "
-	self.errorMsg.SetLabel("Welcome to Mind Sync")
-	global sock
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	host = socket.gethostbyname(sys.argv[1])
-	mPort = 8777
-	sPort = 8778
-	mAddress = (host,mPort)
-	sAddress = (host,sPort)
-	try:
-		sock.connect(mAddress)
+        code = '2'
+        blank = ""
+        print "Trying to login "
+        self.errorMsg.SetLabel("Welcome to Mind Sync")
+    
+        global sock
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = socket.gethostbyname(sys.argv[1])
+    
+        mPort = 8777
+        sPort = 8778
+        mAddress = (host,mPort)
+        sAddress = (host,sPort)
+        try:
+            sock.connect(mAddress)
     	except:
-		sock.connect(sAddress)
+            sock.connect(sAddress)
   
         self.OnConnectInit(code)
-        
+    
+    '''
+        Function to establish connection with server on pressing login button
+    '''
     def OnSignup(self, e):
-	code = '1'
-	blank = ""
-	self.errorMsg.SetLabel("Welcome to Mind Sync")
-	global sock
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	#sock.settimeout(30.0)
-	host = socket.gethostbyname(sys.argv[1])
-	mPort = 8777
-	sPort = 8778
-	mAddress = (host,mPort)
-	sAddress = (host,sPort)
-	try:
-		sock.connect(mAddress)
+        code = '1'
+        blank = ""
+        self.errorMsg.SetLabel("Welcome to Mind Sync")
+    
+        global sock
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = socket.gethostbyname(sys.argv[1])
+    
+        mPort = 8777
+        sPort = 8778
+        mAddress = (host,mPort)
+        sAddress = (host,sPort)
+        try:
+            sock.connect(mAddress)
     	except:
-		sock.connect(sAddress)
+            sock.connect(sAddress)
   
-	self.OnConnectInit(code)        
+        self.OnConnectInit(code)
 
+
+    '''
+        Function for sending server required stats
+    '''
     def OnConnectInit(self,code): 
 	
 	print 'User name: ', self.tc1.GetValue()
@@ -111,7 +124,6 @@ class MainPanel(wx.Frame):
 	sock.recv(1024)
 
         sock.send(serverFormat)
-
         
 	errCode = sock.recv(1024)
 	print "Error code : ", errCode
@@ -167,10 +179,9 @@ class MainPanel(wx.Frame):
 		app2.Show()
         	self.Close(True) 
 
-
-"""
-The second panel that shows the word and the other player's response and takes in the input 
-"""
+'''
+    The second panel that shows the word and the other player's response and takes in the input
+'''
 class SecondPanel(wx.Frame):
 
     def __init__(self, parent, title):
@@ -178,65 +189,62 @@ class SecondPanel(wx.Frame):
             size=(600, 700))
         self.pnl = wx.Panel(self)
 
-	#self.pnl.SetBackgroundColour((237,125,49))
         hbox = wx.BoxSizer(wx.HORIZONTAL)
 
-	print "Second panel"
+        print "Second panel"
 
         fgs = wx.FlexGridSizer(3, 2, 9, 25)
 
 
-	png1 = wx.Image("./index.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
-	wx.StaticBitmap(self.pnl, 1, png1, (5, 5), (png1.GetWidth(), png1.GetHeight()))
+        png1 = wx.Image("./index.png", wx.BITMAP_TYPE_ANY).ConvertToBitmap()
+        wx.StaticBitmap(self.pnl, 1, png1, (5, 5), (png1.GetWidth(), png1.GetHeight()))
 
-	promptSizer = wx.StaticText(self.pnl, -1,
-		label = "Enter the word that pops up in your mind on reading", pos = (20, 240))
-	font = wx.Font(15,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
-	promptSizer.SetFont(font)
-	promptSizer.SetForegroundColour((0,0,255))
+        promptSizer = wx.StaticText(self.pnl, -1, label = "Enter the word that pops up in your mind on reading", pos = (20, 240))
+        font = wx.Font(15,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
+        promptSizer.SetFont(font)
+        promptSizer.SetForegroundColour((0,0,255))
 
-	stats = USER_STATS.split('_')
-	print "Testing " + USER_STATS
+        stats = USER_STATS.split('_')
+        print "Testing " + USER_STATS
 
-	font = wx.Font(10,wx.DECORATIVE, wx.ITALIC, wx.BOLD)
+        font = wx.Font(10,wx.DECORATIVE, wx.ITALIC, wx.BOLD)
 
-	scoreMsg1 = wx.StaticText(self.pnl, label="Total score:   "+str(stats[1]), pos = (350,85))
-	scoreMsg2 = wx.StaticText(self.pnl, label="Highest score: "+str(stats[2]),pos=(350,105))
-	scoreMsg3 = wx.StaticText(self.pnl, label="Lowest score:  "+str(stats[3]), pos=(350,125))
+        scoreMsg1 = wx.StaticText(self.pnl, label="Total score:   "+str(stats[1]), pos = (350,85))
+        scoreMsg2 = wx.StaticText(self.pnl, label="Highest score: "+str(stats[2]),pos=(350,105))
+        scoreMsg3 = wx.StaticText(self.pnl, label="Lowest score:  "+str(stats[3]), pos=(350,125))
 
-	scoreMsg1.SetFont(font)
-
-	scoreMsg2.SetFont(font)
+        scoreMsg1.SetFont(font)
+        
+        scoreMsg2.SetFont(font)
 	
-	scoreMsg3.SetFont(font)
+        scoreMsg3.SetFont(font)
 
 	
-	response = sock.recv(1024)
+        response = sock.recv(1024)
 
-	words = response.split("_")
+        words = response.split("_")
 
-	wrd = words[0]
+        wrd = words[0]
 
-	print "Word: " + wrd
+        print "Word: " + wrd
 	
-	self.wordSizer = wx.StaticText(self.pnl, -1,label = wrd, pos = (220, 300))
-	font = wx.Font(25,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
-	self.wordSizer.SetFont(font)
-	self.wordSizer.SetForegroundColour((255,0,0))
+        self.wordSizer = wx.StaticText(self.pnl, -1,label = wrd, pos = (220, 300))
+        font = wx.Font(25,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
+        self.wordSizer.SetFont(font)
+        self.wordSizer.SetForegroundColour((255,0,0))
 	
-	enterbut = wx.Button(self.pnl, label='Enter', pos=(100, 500), size = (100,50))
+        enterbut = wx.Button(self.pnl, label='Enter', pos=(100, 500), size = (100,50))
 
-	exitbut = wx.Button(self.pnl, label='End Game', pos=(300, 500), size = (100,50))
+        exitbut = wx.Button(self.pnl, label='End Game', pos=(300, 500), size = (100,50))
 
-	self.word = wx.TextCtrl(self.pnl, style = wx.TE_PROCESS_ENTER, pos = (175,400), 
-	size = (200,50))
+        self.word = wx.TextCtrl(self.pnl, style = wx.TE_PROCESS_ENTER, pos = (175,400),	size = (200,50))
 	
-	hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
+        hbox.Add(fgs, proportion=1, flag=wx.ALL|wx.EXPAND, border=15)
         self.pnl.SetSizer(hbox)
         self.Centre()
         self.Show()
 	
-	scoreString = "Score: "+words[1]
+        scoreString = "Score: "+words[1]
         self.scoreSizer = wx.StaticText(self.pnl, -1,
         label = scoreString , pos = (300, 600))
         scorefont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
@@ -248,16 +256,6 @@ class SecondPanel(wx.Frame):
         responsefont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
         self.responseSizer.SetFont(responsefont)
 
-		
-	'''self.connectionErrorMsg = wx.StaticText(self.pnl,-1, label="Connected!!", pos = (300, 550))		
-	connectionErrorFont = wx.Font(15,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
-	self.connectionErrorMsg.SetFont(connectionErrorFont)
-	self.connectionErrorMsg.SetForegroundColour((255,0,0))
-	
-	self.gameEndErrorMsg = wx.StaticText(self.pnl, -1, label="Keep Playing!!!", pos = (15, 550))
-	gameEndFont = wx.Font(15,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
-	self.gameEndErrorMsg.SetFont(gameEndFont)
-	self.gameEndErrorMsg.SetForegroundColour((255,0,0))'''
 
 	enterbut.Bind(wx.EVT_BUTTON , self.OnEnterPress)
 
@@ -294,8 +292,6 @@ class SecondPanel(wx.Frame):
                 response = sock.recv(1024)
 	    except:
                 print "receive error or timedout"
-                #self.connectionErrorMsg.SetLabel("Connection error")
-                #self.connectErrorMsg.SetForegroundColour((255,0,0))
                 time.sleep(5)
 		sys.exit(0)
 
@@ -309,7 +305,6 @@ class SecondPanel(wx.Frame):
                 time.sleep(1)
                 sys.exit(0)
 
-            #wrd = "GAMEEND"        
             if (wrd == "GAMEEND"):
                 print "received GAMEEND"
 	
@@ -317,10 +312,6 @@ class SecondPanel(wx.Frame):
                 words = response.split("_")
                 wrd = words[0]
                 print "receive end" + wrd
-                '''response = sock.recv(1024)
-                words = response.split("_")
-                wrd = words[0]
-                print "receive end" + wrd'''
 
             self.wordSizer.SetLabel(wrd)
             font = wx.Font(25,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
@@ -335,7 +326,9 @@ class SecondPanel(wx.Frame):
             self.responseSizer.SetLabel(words[2]+"'s response: "+words[3])
             responsefont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
 
-
+    '''
+    Function for sending correct info to server on pressing enter button
+    '''
     def OnEnterPress(self, e):
 	print "Entered word: ", self.word.GetValue()
 	sock.send(self.word.GetValue())
@@ -349,9 +342,6 @@ class SecondPanel(wx.Frame):
 	try:
 		response = sock.recv(1024)
 	except:
-       		print "receive error or timedout"	
-		#self.connectionErrorMsg.SetLabel("Connection error")
-		#self.connectErrorMsg.SetForegroundColour((255,0,0))
 		time.sleep(5)
 		sys.exit(0)
 
@@ -365,25 +355,12 @@ class SecondPanel(wx.Frame):
 		time.sleep(1)	
 		sys.exit(0)
 	
-        #wrd = "GAMEEND"	
 	if (wrd == "GAMEEND"):
-       		print "received GAMEEND"	
-		'''gameErrormsg = "Your partner has closed the game, the game will exit now!!!"
-		self.gameEndErrorMsg.SetLabel(gameErrormsg)
-		gameEndFont = wx.Font(15,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
-		self.gameEndErrorMsg.SetFont(gameEndFont)
-		self.gameEndErrorMsg.SetForegroundColour((255,0,0))
-		#	time.sleep(5)
-		self.pnl.Destroy()
-	        sys.exit(0)'''
+       		print "received GAMEEND"
 		response = sock.recv(1024)
 		words = response.split("_")
 		wrd = words[0]
 		print "receive end" + wrd
-		'''response = sock.recv(1024)
-		words = response.split("_")
-		wrd = words[0]
-		print "receive end" + wrd'''
 	
 	self.wordSizer.SetLabel(wrd)
 	font = wx.Font(25,wx.DECORATIVE, wx.ITALIC, wx.NORMAL)
@@ -399,7 +376,9 @@ class SecondPanel(wx.Frame):
         responsefont = wx.Font(15,wx.DECORATIVE, wx.NORMAL, wx.NORMAL)
         self.responseSizer.SetFont(responsefont)
 
-
+    '''
+        Function for graceful shutdown on pressing exit button
+    '''
     def OnExitPress(self, e):
 	print "Exiting"
 	try:
@@ -410,6 +389,10 @@ class SecondPanel(wx.Frame):
 	finally:
 		self.Close(True)
 
+
+'''
+    The main function
+'''
 if __name__ == '__main__':
     app = wx.App()
     MainPanel(None, title='Mind Sync')
